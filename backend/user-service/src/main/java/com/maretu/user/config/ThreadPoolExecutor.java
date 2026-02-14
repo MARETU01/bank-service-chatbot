41a4e30f21ee2c3b6835b4a292ba9f9b0327c755
luoding <2636064351@qq.com>
2026-02-14 17:19:41 +0800

    update

---
+++ backend/user-service/src/main/java/com/maretu/user/config/ThreadPoolExecutor.java
@@ -0,0 +1,15 @@
+package com.maretu.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ThreadPoolExecutor {
+
+    @Bean(name = "virtualThreadPoolExecutor")
+    public Executor virtualThreadPoolExecutor() {
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
+}
--- backend/user-service/src/main/java/com/maretu/user/service/impl/UsersServiceImpl.java
+++ backend/user-service/src/main/java/com/maretu/user/service/impl/UsersServiceImpl.java
@@ -1,21 +1,19 @@
 package com.maretu.user.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.maretu.common.dto.Context;
+import com.maretu.common.utils.JwtUtils;
+import com.maretu.user.enums.UserStatus;
 import com.maretu.user.mapper.UsersMapper;
 import com.maretu.user.pojo.Users;
 import com.maretu.user.service.IUsersService;
+import com.maretu.user.utils.HashUtil;
 import com.maretu.user.utils.MailUtil;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author
- * @since 2025-04-22
- */
+import java.util.Objects;
+
 @Service
 public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements IUsersService {
 
@@ -29,6 +27,21 @@ public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements
 
     @Override
     public String login(Users user) {
-    return null;
+        Users userToLogin = lambdaQuery()
+                .eq(user.getEmail() != null, Users::getEmail, user.getEmail())
+                .or()
+                .eq(user.getPhone() != null, Users::getPhone, user.getPhone())
+                .one();
+        if (userToLogin == null || !HashUtil.checkPassword(user.getPassword(), userToLogin.getPassword())) {
+            throw new RuntimeException("email/phone or password not correct");
+        }
+        if (!Objects.equals(userToLogin.getStatus(), UserStatus.NORMAL.getCode())) {
+            throw new RuntimeException("user's status is not active");
+        }
+        Context context = new Context();
+        context.setUserId(Math.toIntExact(userToLogin.getId()))
+                .setUsername(userToLogin.getUsername())
+                .setEmail(userToLogin.getEmail());
+        return JwtUtils.generateJwt(context);
     }
 }

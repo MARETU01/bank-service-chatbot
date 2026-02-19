0e37f7cc280eb54622486700c176a5166ec5f16c
luoding <2636064351@qq.com>
2026-02-19 14:12:28 +0800

    前端删除#

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '../components/Layout.vue'
 
 const routes = [
@@ -60,7 +60,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 

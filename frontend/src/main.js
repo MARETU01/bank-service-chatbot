7bd4470ae44b624cbbe52ebcb2ed092802ab8a3f
luoding <2636064351@qq.com>
2026-02-21 14:02:28 +0800

    前端挂载http

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import http from './http'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.config.globalProperties.$http = http
+
+app.use(store).use(router).mount('#app')

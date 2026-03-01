b5aabf364003f900c677746f83a53d1935f0e6a8
luoding <2636064351@qq.com>
2026-03-01 11:00:40 +0800

    初始化ChatController

---
+++ backend/chatbot-service/src/main/java/com/maretu/chat/controller/ChatController.java
@@ -0,0 +1,11 @@
+package com.maretu.chat.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+}

db78ce0f271bc1befccdf9581856ae3d06589f8a
luoding <2636064351@qq.com>
2026-03-17 21:12:33 +0800

    chat-service

--- backend/chatbot-service/src/main/java/com/maretu/chat/controller/ChatController.java
+++ backend/chatbot-service/src/main/java/com/maretu/chat/controller/ChatController.java
@@ -9,7 +9,6 @@ import com.maretu.chat.service.ISessionService;
 import com.maretu.common.dto.Context;
 import com.maretu.common.utils.Result;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
@@ -23,7 +22,6 @@ public class ChatController {
     private final ISessionService sessionService;
     private final IMessageService messageService;
     private final ObjectMapper jacksonObjectMapper;
-    private final ChatClient chatClient;
 
     @GetMapping("/session")
     public Result<List<Session>> getSessions(@RequestHeader("user-info") String userJson) throws JsonProcessingException {

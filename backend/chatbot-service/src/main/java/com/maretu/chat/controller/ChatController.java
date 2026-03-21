a958873732e63d1606b62cd43cc01c764550fb16
luoding <2636064351@qq.com>
2026-03-21 16:06:05 +0800

    embed model

--- backend/chatbot-service/src/main/java/com/maretu/chat/controller/ChatController.java
+++ backend/chatbot-service/src/main/java/com/maretu/chat/controller/ChatController.java
@@ -9,9 +9,12 @@ import com.maretu.chat.service.ISessionService;
 import com.maretu.common.dto.Context;
 import com.maretu.common.utils.Result;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class ChatController {
     private final ISessionService sessionService;
     private final IMessageService messageService;
     private final ObjectMapper jacksonObjectMapper;
+    private final OpenAiEmbeddingModel openAiEmbeddingModel;
 
     @GetMapping("/session")
     public Result<List<Session>> getSessions(@RequestHeader("user-info") String userJson) throws JsonProcessingException {
@@ -81,5 +85,12 @@ public class ChatController {
     public Flux<String> chat(@RequestHeader("user-info") String userJson,
                              @RequestBody Message message) throws JsonProcessingException {
         return messageService.chat(userJson, message);
+    }
+
+    @GetMapping("/test")
+    public Result<Void> test() {
+        float[] helloWorlds = openAiEmbeddingModel.embed("hello world");
+        System.out.println("hello world embedding: " + Arrays.toString(helloWorlds));
+        return Result.success(null);
     }
 }

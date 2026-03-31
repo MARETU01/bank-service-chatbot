7c6d798455eca783dd3d39f7b428326f16983807
luoding <2636064351@qq.com>
2026-03-31 21:02:02 +0800

    向量数据库

--- docker/redis/start.sh
+++ docker/redis/start.sh
@@ -2,6 +2,7 @@ docker run -d \
   --name redis-stack \
   -p 6379:6379 \
   -p 8001:8001 \
+  -v ./data:/data \
   -e REDIS_ARGS="--requirepass maretu" \
   --network maretu \
   redis/redis-stack:latest
